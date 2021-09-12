fa91c2f2aac4696c9028e940ffe1c60dec60d97f
Aishwarya Jayagopal <jayagopalaishwarya@gmail.com>
2021-09-12 06:02:55 +0000

    Added user registration page

--- frontend/public/scripts/login_page.js
+++ frontend/public/scripts/login_page.js
@@ -1,8 +1,31 @@
 const userlogin_submit = document.getElementById("user_login_submit");
 userlogin_submit.addEventListener("click",submitted_login_details);
 
+// placeholder for users - to be fetched from DB later
+const users_dict = {
+	"aishwarya@gmail.com": "trial1234",
+	"testuser@gmail.com": "trial1234"
+};
+
 function submitted_login_details(){
 	const userName = document.querySelector(".user_email_id");
 	const userPassword = document.querySelector(".user_password");
-	alert(userPassword.value);
+	console.log(userName.value);
+	if (userName.value in users_dict){
+		if (users_dict[userName.value] != userPassword.value){
+			alert("Incorrect credentials!");
+			return;
+		}
+		else{
+			display_user_dashboard();
+		}
+	}
+	else{
+		alert("No such user!");
+	}
+}
+
+function display_user_dashboard(){
+	alert("Take user to user dashboard");
+	return;
 }
---
+++ frontend/public/scripts/register_user_page.js
@@ -0,0 +1,6 @@
+const create_user_submit = document.getElementById("create_user_submit");
+create_user_submit.addEventListener("click",create_new_user);
+
+function create_new_user(){
+	alert("Register New User!");
+}
